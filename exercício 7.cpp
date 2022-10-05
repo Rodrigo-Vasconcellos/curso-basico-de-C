297cf8fc67588013ae9c287d9682dec8df702967
Rodrigo Vasconcellos <rodrigo-jaka@hotmail.com>
2022-10-05 15:03:27 -0300

    exercicio 7

--- exercício 7.cpp
+++ exercício 7.cpp
@@ -1,4 +1,6 @@
-// exercicio 7 
+// exercicio 7  Crie um algoritmo que leia o nome e as duas notas de um aluno, calcule a sua 
+// média e mostre na tela. No final, analise a média e mostre se o aluno teve ou 
+// não um bom aproveitamento (se ficou acima da média 7.0).
 
 #include <stdio.h> //funçao de entrada e saida
 #include <locale.h> //funçao de caracter
@@ -6,8 +8,20 @@
 int main () {
 	setlocale(LC_ALL, "portuguese");
 	
-	
-	
-	
+	char nome;
+	float n1, n2, media;
+	printf("digite seu nome: ");
+	scanf("%10c", &nome);
+	printf("\ndigite 1° nota: ");
+	scanf("%f", &n1);
+	printf("\ndigite 2° nota: ");
+	scanf("%f", &n2);
+	media = (n1+n2)/2;
+	if (media >= 7.0) {
+		printf("\naprovado!!!\n sua media : %.2f", media);
+	}
+	else{
+		printf("\nreprovado!!!\n sua media : %.2f", media);
+	}	
 }
 
