807e8c662db8b409e20de070045941cc397d0d4f
Rodrigo Vasconcellos <rodrigo-jaka@hotmail.com>
2022-10-21 15:54:08 -0300

    exercício 13

---
+++ exercícios/exercício 13 fibonacci com procedimento.cpp
@@ -0,0 +1,32 @@
+//Desenvolva um aplicativo que recebe um único valor inteiro como parâmetro, indicando quantos 
+//termos da sequência serão mostrados na tela. O seu procedimento deve receber 
+//esse valor e mostrar a quantidade de elementos solicitados.
+//Ex: 
+//Fibonacci(5) vai gerar 1 >> 1 >> 2 >> 3 >> 5 >> FIM
+//Fibonacci(9) vai gerar 1 >> 1 >> 2 >> 3 >> 5 >> 8 >> 13 >> 21 >> 34 >> FIM
+
+#include <stdio.h>
+#include <locale.h>
+
+int main () {
+	setlocale (LC_ALL, "portuguese");
+	int a, b, c, soma, quantidadeDeTermos, i;
+	
+	a = 1;
+	b = 0;
+	c = 0;
+	soma = 0;
+	printf ("dgite um valor inteiro para delimitar quantos termos da sequancia de fibonacci vai aparecer na tela: ");
+	scanf ("%d", &quantidadeDeTermos);
+	for (i=1; i<=quantidadeDeTermos; i++){
+		soma = a+b;
+		c = soma;
+		a = b;
+		b = soma;
+		printf ("%d >>", c);	
+	}
+	printf ("fim");
+}
+
+
+	
